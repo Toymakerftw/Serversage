d89e17d1c39a1422d7bc13277e51607022d0df86
Toymakerftw <cv.toymakerftw@gmail.com>
2022-06-30 23:25:21 +0530

    Navbar: Inital Navbar

---
+++ navbar.php
@@ -0,0 +1,7 @@
+<ul class="floating-menu">
+    <li><a href="shell.php">TERMINAL</a></li>
+    <li><a href="fileupload.php">FTP</a></li>
+    <li><a href="network.php">NETWORK</a></li>
+    <li><a href="/logout.php">LOGOUT</a></li>
+    <li><a href="/about.html">ABOUT</a></li>
+</ul>
