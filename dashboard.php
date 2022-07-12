fda244fdb75e8ec7bd5efe9c602122809443cb9e
Toymakerftw <cv.toymakerftw@gmail.com>
2022-07-13 01:34:40 +0530

    Dashboard: Modal: Add device count & minor cleanup

--- dashboard.php
+++ dashboard.php
@@ -29,8 +29,12 @@ if ($connection->connect_error) {
 }
 // read all row from database table
 $sql = "SELECT COUNT(*) FROM currentiptable";
+$sql1 = "SELECT COUNT(*) FROM knowiptable";
 $result = $connection->query($sql);
+$result1 = $connection->query($sql1);
+
 $row = mysqli_fetch_row($result);
+$row1 = mysqli_fetch_row($result1);
 
 if (!$result) {
   die("Invalid query: " . $connection->error);
@@ -72,7 +76,7 @@ setInterval(get_data, 500);
 
 
 <div class="header">
-  <div>
+ <div id="subgrid" class="subgrid">
   <p>
   <?php
     //echo'<p class="sheader">Currently logged in as : ', $_SESSION['usrname'],'</p>';
@@ -83,7 +87,7 @@ setInterval(get_data, 500);
 </div>
 
 <div id="grid" class="grid">
-  <div>
+ <div id="subgrid" class="subgrid">
   <p id="cpu_temp" class="gridheader">
     <?php
     echo $temp ;
@@ -91,25 +95,29 @@ setInterval(get_data, 500);
     ?>
     </p>
   <p class="footer">Cpu Temprature</p>
-  <p class="modalfooter" id="myBtn"> Show Graph</p>
+  <div id="footerbutton" class="footerbutton">
+  <button class="modal-button" href="#myModal1">Show Graph</button>
+  </div>
 
   </div>
-  <div>
+ <div id="subgrid" class="subgrid">
   <p  id="cpu_load" class="gridheader">
     <?php
     echo "$load\n";
     ?>
     </p>
     <p class="footer">CPU Usage</p>
-    <p class="modalfooter" id="cpuusg"> Show Graph</p>
+    <div id="footerbutton" class="footerbutton">
+    <button class="modal-button" href="#myModal2">Show Graph</button>
+    </div> 
     </div>
 
-  <div>
+ <div id="subgrid" class="subgrid">
   <p id="mem_usd"  class="gridheader"></p>
   <p id="mem_tlt" class="gridheader2"></p>
   <p class="footer">Memory Usage</p>
   </div>
-  <div>
+ <div id="subgrid" class="subgrid">
   <p  class="gridheader">
     <?php
     echo $up;
@@ -117,7 +125,7 @@ setInterval(get_data, 500);
     </p>
     <p class="footer">System Uptime</p>
   </div>
-  <div>
+ <div id="subgrid" class="subgrid">
   <p  id="disk_usd" class="gridheader">
   <p   class="gridsubheader"> Used of</p>
   <p  id="disk_tlt" class="gridheader2">
@@ -129,7 +137,7 @@ setInterval(get_data, 500);
     </p>
     <p class="footer">Disk Usage</p>
     </div>
-    <div>
+   <div id="subgrid" class="subgrid">
     <p  class="gridheader">
     <?php
     echo "$net[0]\n";
@@ -138,7 +146,7 @@ setInterval(get_data, 500);
     </p>
     <p class="footer">Network Speed</p>
     </div>
-  <div>
+ <div id="subgrid" class="subgrid">
   <p  class="gridheader">
     <?php
     echo $os ;
@@ -146,7 +154,7 @@ setInterval(get_data, 500);
     </p>
     <p class="footer">Operating System</p>
   </div>
-  <div>
+ <div id="subgrid" class="subgrid">
   <p  class="gridheader">
     <?php
     echo $pro;
@@ -154,97 +162,136 @@ setInterval(get_data, 500);
     </p>
     <p class="footer">Cpu</p>
   </div>
-  <div>
+ <div id="subgrid" class="subgrid">
   <p  class="gridheader">
     <?php
     echo $row[0];
     ?>
     </p>
-    <?php if ($row[0] = "1") 
+    <?php if ($row[0] == 1) 
     {
-      echo '<p class="footer">Connected Device</p>';
+      echo "<p class='footer'>Connected Device</p>";
     }
     else 
     {
-      echo '<p class="footer">Connected Devices</p>';
+      echo "<p class='footer'>Connected Devices</p>";
     }?>
+    <div id="footerbutton" class="footerbutton">
+    <button class="modal-button" href="#myModal3">Show More</button>
+    </div>
     </div>
 </div>
 </div>
 
-<!-- The Modal -->
-<div id="myModal" class="modal">
+<!-- The CPU TEMP Modal -->
+<div id="myModal1" class="modal">
 
   <!-- Modal content -->
   <div class="modal-content">
-    <span class="close">&times;</span>
-    <p>Some text in the Modal..</p>
-    <p>Some text in the Modal..</p>
+      <span class="close">×</span>
+    <div class="modal-body">
+      <p>Some text in the Modal 1</p>
+      <p>Some other text...</p>
+    </div>
   </div>
 </div>
 
-<!-- The Modal -->
-<div id="cpuUsageModal" class="modal">
+<!-- The CPU USAGE Modal -->
+<div id="myModal2" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+      <span class="close">×</span>
+    <div class="modal-body">
+      <p>Some text in the Modal 2</p>
+      <p>Some other text...</p>
+    </div>
+  </div>
+
+</div>
+
+<!-- The Connected Devices Modal -->
+<div id="myModal3" class="modal">
 
   <!-- Modal content -->
   <div class="modal-content">
     <span class="close">&times;</span>
-    <p>Some text in the Modal..</p>
-    <p>Some text in the Modal..</p>
+    <?php 
+        $ukndev=($row[0] - $row1[0]);
+
+    echo '
+    <div id="gridModal" class="gridModal">  
+   <div id="subgrid" class="subgrid">
+    <p  class="gridheader">';
+    echo $ukndev ;
+    echo '  
+    </p>';
+    if ($ukndev[0] == 1) 
+    {
+      echo '<p class="footer">Unknown Device</p>';
+    }
+    else 
+    {
+      echo '<p class="footer">Unknown Devices</p>';
+    }
+    echo '</div>';
+
+    echo '  
+   <div id="subgrid" class="subgrid">
+    <p  class="gridheader">';
+    echo $row1[0] ;
+    echo '  
+    </p>';
+    if ($row1[0] == 1) 
+    {
+      echo '<p class="footer">Known Device</p>';
+    }
+    else 
+    {
+      echo '<p class="footer">Known Devices</p>';
+    }
+    echo '</div> </div>';
+    ?>
   </div>
 </div>
 
 <script>
+
 // Get the modal
-var modal = document.getElementById("myModal");
-
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.querySelectorAll("button.modal-button");
+
+// All page modals
+var modals = document.querySelectorAll('.modal');
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var spans = document.getElementsByClassName("close");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+ }
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-var modal = document.getElementById("cpuUsageModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("cpuusg");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-cpuusg.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
 }
 
 </script>
