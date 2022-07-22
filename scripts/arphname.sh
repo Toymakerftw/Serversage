eec4f237bc9d4f3af0ebdb12c85103100795a11b
Toymakerftw <cv.toymakerftw@gmail.com>
2022-07-22 14:43:08 +0530

    Scripts: Let arp scan the whole network

--- scripts/arphname.sh
+++ scripts/arphname.sh
@@ -1,5 +1,5 @@
 ip=$(ifconfig | grep 255.255 | awk '{print $2}')
-nip=$(sed 's/.\{3\}$//' <<< "$ip")
+nip=$(sed 's/.\{6\}$//' <<< "$ip")
 arp=($(arp -a | grep $nip | awk '{print $1}' | sed 's/)//' | sed 's/(//'))
 len=${#arp[@]}
 for (( i=0; i<$len; i++)); do
