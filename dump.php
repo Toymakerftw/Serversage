ef8148ab7b8d73d79891702abc96e2507a3de2f0
Toymakerftw <cv.toymakerftw@gmail.com>
2022-07-22 14:45:30 +0530

    Network: Let tcpdump redirect to network page after dump.

--- dump.php
+++ dump.php
@@ -4,9 +4,15 @@
     $ip = $_GET['ip']; // Assigning GET variable 'ip.'
     exec("bash scripts/dump.sh $ip 2>&1",$output);
     $i=count($output);
-    while ($i >= 0)
+    if ($i >= 0) {
+        header("Location: network.php");
+        die();    }
+    else {
+        echo "failed";
+    }
+    /*while ($i >= 0)
     {
         echo $output[$i];
         $i--;
-    }
+    }*/
 ?>
