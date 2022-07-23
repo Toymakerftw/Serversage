bd1b25af23b79eda730e31e37600729418bb7e96
Toymakerftw <cv.toymakerftw@gmail.com>
2022-07-23 18:26:40 +0530

    Network: Let tcpdump captures from all interfaces

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -1,6 +1,6 @@
 array=("$@")
 #echo $array;
-dump=($(tcpdump -i wlo1 host $array -c 5 -w dump/dump.pcap))
+dump=($(tcpdump -i any host $array -c 5 -w dump/dump.pcap))
 #len=${#dump[@]}
 #for (( i=0; i<$len; i++)); do
 #echo "${dump[$i]}"
